3e4a6580dbd90c7c186fae0eb24ce1ab374b097b
Palloma <tiodosiop@gmail.com>
2026-05-20 12:13:16 -0300

    Adicionando Models: Aula (20/05/2026)

--- Projeto Login/Projeto Login/Projeto Login/Repository/ClienteRepository.cs
+++ Projeto Login/Projeto Login/Projeto Login/Repository/ClienteRepository.cs
@@ -1,11 +1,15 @@
-﻿using MySql.Data.MySqlClient;
+﻿using System.ComponentModel.DataAnnotations;
+using System.Data;
+using MySql.Data.MySqlClient;
+using Org.BouncyCastle.Tls;
 using Projeto_Login.Models;
+using Projeto_Login.Models.Constant;
 using Projeto_Login.Repository.Contract;
 using X.PagedList;
 
 namespace Projeto_Login.Repository
 {
-    public class ClienteRepository //:// IClienteRepository
+    public class ClienteRepository : IClienteRepository
     {
         private readonly string _conexaoMySQL;
         IConfiguration _conf;
@@ -18,17 +22,65 @@ namespace Projeto_Login.Repository
 
         public void Atualizar(Cliente cliente)
         {
-            throw new NotImplementedException();
+            string Situacao = SituacaoConstant.Ativo;
+            using (var conexao = new MySqlConnection(_conexaoMySQL))
+            {
+                conexao.Open();
+                MySqlCommand cmd = new MySqlCommand("update Cliente set Nome=@Nome, Nascimento=@Nascimento, Sexo=@Sexo, CPF=@CPF," +
+                    "Telefone=@Telefone, Email=@Email, Senha=@Senha, Situacao=@Situacao WHERE Id=@Id", conexao);
+
+                cmd.Parameters.Add("@ID", MySqlDbType.VarChar).Value = cliente.ID;
+                cmd.Parameters.Add("@Nome", MySqlDbType.VarChar).Value = cliente.Nome;
+                cmd.Parameters.Add("@Nascimento", MySqlDbType.DateTime).Value = cliente.Nascimento.ToString("yyyy/MM/dd");
+                cmd.Parameters.Add("@Sexo", MySqlDbType.VarChar).Value = cliente.Sexo;
+                cmd.Parameters.Add("@CPF", MySqlDbType.VarChar).Value = cliente.CPF;
+                cmd.Parameters.Add("@Telefone", MySqlDbType.VarChar).Value = cliente.Telefone;
+                cmd.Parameters.Add("@Email", MySqlDbType.VarChar).Value = cliente.Email;
+                cmd.Parameters.Add("@Senha", MySqlDbType.VarChar).Value = cliente.Senha;
+                cmd.Parameters.Add("@Situacao", MySqlDbType.VarChar).Value = Situacao;
+
+                cmd.ExecuteNonQuery();
+                conexao.Close();
+            }
         }
+        
 
         public void Cadastrar(Cliente cliente)
         {
-            throw new NotImplementedException();
+            string Situacao = SituacaoConstant.Ativo;
+
+            using (var conexao = new MySqlConnection(_conexaoMySql))
+            {
+                conexao.Open();
+
+                MySqlCommand cmd = new MySqlCommand("insert into Cliente(Nome,Nacimento, Sexo, CPF, Telefone, Email, Senha, Situacao)" +
+                     " values (@Name, @Nascimento, @Sexo, @CPF, @Telefone, @Email, @Senha, @Situacao)", conexao); //@: PARAMETRO
+
+                cmd.Parameters.Add("@Nome", MySqlDbType.VarChar).Value = cliente.Nome;
+                cmd.Parameters.Add("@Nascimento", MySqlDbType.DateTime).Value = cliente.Nascimento.ToString("yyyy/MM/dd");
+                cmd.Parameters.Add("@Sexo", MySqlDbType.VarChar).Value = cliente.Sexo;
+                cmd.Parameters.Add("@CPF", MySqlDbType.VarChar).Value = cliente.CPF;
+                cmd.Parameters.Add("@Telefone", MySqlDbType.VarChar).Value = cliente.Telefone;
+                cmd.Parameters.Add("@Email", MySqlDbType.VarChar).Value = cliente.Email;
+                cmd.Parameters.Add("@Senha", MySqlDbType.VarChar).Value = cliente.Senha;
+                cmd.Parameters.Add("@Situacao", MySqlDbType.VarChar).Value = Situacao;
+
+                cmd.ExecuteNonQuery();
+                conexao.Close();
+            }
         }
 
         public void Excluir(int Id)
         {
-            throw new NotImplementedException();
+            using (var conexao = new MySqlConnection(_conexaoMySQL))
+            {
+                conexao.Open();
+                MySqlCommand cmd = new MySqlCommand("delete from Cliente WHERE ID=@ID", conexao);
+                cmd.Parameters.AddWithValue("@ID", Id);
+                int i = cmd.ExecuteNonQuery();
+                conexao.Close();
+
+            }
         }
 
         public Cliente Login(string Email, string Senha)
@@ -48,14 +100,14 @@ namespace Projeto_Login.Repository
 
                 while (dr.Read())
                 {
-                    //cliente.Id = Convert.ToInt32(dr["Id"]);
+                    cliente.ID = Convert.ToInt32(dr["ID"]);
                     cliente.Nome = Convert.ToString(dr["Nome"]);
                     cliente.Nascimento = Convert.ToDateTime(dr["Nascimento"]);
 
                     cliente.Sexo = Convert.ToString(dr["Sexo"]);
                     cliente.CPF = Convert.ToString(dr["CPF"]);
                     cliente.Telefone = Convert.ToString(dr["Telefone"]);
-                   // cliente.Situacao = Convert.ToString(dr["Situacao"]);
+                    cliente.Situacao = Convert.ToString(dr["Situacao"]);
 
                     cliente.Email = Convert.ToString(dr["Email"]);
                     cliente.Senha = Convert.ToString(dr["Senha"]);
@@ -68,18 +120,113 @@ namespace Projeto_Login.Repository
 
         public Cliente ObterCliente(int Id)
         {
-            throw new NotImplementedException();
+            using (var conexao = new MySqlConnection(_conexaoMySQL.MySql))
+            {
+                conexao.Open();
+                MySqlCommand cmd = new MySqlCommand("select * from Cliente WHERE  id=@id", conexao);
+                cmd.Paramaters.AddWithValue("@Id", Id);
+
+                MySqlDataAdapter da = new MySqlDataAdapter(cmd);
+                MySqlDataReader dr;
+
+                Cliente cliente = new Cliente();
+                dr = cmd.ExecuteReader(CommandBehavior.CloseConnection);
+                while (dr.Read())
+                {
+                    cliente.ID = (Int32)(dr["Id"]);
+                    cliente.Nome = (string)(dr["Nome"]);
+                    cliente.Nascimento = (DateTime)(dr["Nascimento"]);
+                    cliente.Sexo = (string)(dr["Sexo"]);
+                    cliente.CPF = (string)(dr["CPF"]);
+                    cliente.Telefone = (string)(dr["Telefone"]);
+                    cliente.Email = (string)(dr["Email"]);
+                    cliente.Senha = (string)(dr["Senha"]);
+                    cliente.Situacao = (string)(dr["Situacao"]);
+
+
+
+
+                }
+                return cliente;
+
+            }
         }
 
-      //  public IEnumerable<Cliente> //ObterTodosClientes()
-       // {
-           // List<Cliente> cList = new List<Cliente> ();
-            //using (var conexao = new MySqlConnection )
-       // }
+        public IEnumerable<Cliente> ObterTodosClientes()
+        {
+            List<Cliente> cList = new List<Cliente> ();
+            using (var conexao = new MySqlConnection )
+            {
+                conexao.Open();
+
+                MySqlCommand cmd = new MySqlCommand("select * from cliente where Email = @Email and Senha = @Senha", conexao);
+
+                cmd.Parameters.Add("@Email", MySqlDbType.VarChar).Value = Email;
+                cmd.Parameters.Add("@Senha", MySqlDbType.VarChar).Value = Senha;
+
+                MySqlDataAdapter da = new MySqlDataAdapter(cmd);
+                MySqlDataReader dr;
+
+                Cliente cliente = new Cliente();
+                dr = cmd.ExecuteReader(CommandBehavior.CloseConnection);
+
+                while (dr.Read())
+                {
+                    cliente.Id = Convert.ToInt32(dr["id"]);
+                    cliente.Nome = Convert.ToString(dr["Nome"]);
+                    cliente.Nascimento = Convert.ToDateTime(dr["Nascimento"]);
+
+                    cliente.Sexo = Convert.ToString(dr["Sexo"]);
+                    cliente.CPF = Convert.ToString(dr["CPF"]);
+                    cliente.Telefone = Convert.ToString(dr["Telefone"]);
+                    cliente.Email = Convert.ToString(dr["Email"]);
+                    cliente.Senha = Convert.ToString(dr["Senha"]);
+                    cliente.Situacao = Convert.ToString(dr["Situacao"]);
+
+                }
+
+
+
+
+            }
+        }
 
         public IPagedList<Cliente> ObterTodosClientes(int? pagina, string pesquisa)
         {
-            throw new NotImplementedException();
+            List<Cliente> cliList = new List<Cliente>();
+            using (var conexao = new MySqlCommand(_conexaoMySQL))
+            {
+                conexao.Open();
+                MySqlCommand cmd = new MySqlCommand("SELECT * FROM CLIENTE", conexao);
+                MySqlDataAdapter da = new MySqlDataAdapter(cmd);
+                DataTable dt = new DataTable();
+
+                da.Fill(dt);
+
+                conexao.Clone();
+
+                foreach (DataRow dr in dt.Rows)
+                {
+                    cliList.Add
+                     (
+                        new Cliente
+                        {
+                            Id = Convert.ToInt32(dr["id"]),
+                            Nome = (string)(dr["Nome"]),
+                            Nascimento = Convert.ToDateTime(dr["Nascimento"]),
+                            Sexo = Convert.ToString(dr["Sexo"]),
+                            CPF = Convert.ToString(dr["CPF"]),
+                            Telefone = Convert.ToString(dr["Telefone"]),
+                            Email = Convert.ToString(dr["Email"]),
+                            Senha = Convert.ToString(dr["Senha"]),
+                            Situacao = Convert.ToString(dr["Situacao"]),
+                        }
+                        
+                     );
+
+                }
+                return cliList;
+            }
         }
     }
 
--- Projeto Login/Projeto Login/Projeto Login/obj/Debug/net8.0/Projeto Login.AssemblyInfo.cs
+++ Projeto Login/Projeto Login/Projeto Login/obj/Debug/net8.0/Projeto Login.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Projeto Login")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ea5993abb052355280d5111a3eeec46b1c6bce0f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fdeaed50c5dc0735f27adcc757db4d1a4032d5d0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Projeto Login")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Projeto Login")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
